686db552a35ddc85680e98c89ff21cef2cb8c182
Matt Hill <mattnine@protonmail.com>
2026-03-24 17:51:36 -0600

    sdk beta.66 + version bump

--- startos/init/initializeService.ts
+++ startos/init/initializeService.ts
@@ -4,13 +4,12 @@ import { i18n } from '../i18n'
 import { sdk } from '../sdk'
 
 export const initializeService = sdk.setupOnInit(async (effects, kind) => {
-  if (kind !== 'install') return
-
-  // Seed store with defaults
   await storeJson.merge(effects, {})
 
-  // Prompt user to configure their API key
-  await sdk.action.createOwnTask(effects, configure, 'important', {
-    reason: i18n('Set your Maple API key'),
-  })
+  if (kind === 'install') {
+    // Prompt user to configure their API key
+    await sdk.action.createOwnTask(effects, configure, 'important', {
+      reason: i18n('Set your Maple API key'),
+    })
+  }
 })
--- startos/install/versions/index.ts
+++
@@ -1,2 +0,0 @@
-export { v_0_1_7_4_b3 as current } from './v_0_1_7_4_b3'
-export const other = []
